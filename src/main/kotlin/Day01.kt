2beb365d0c7c664ff8c3997dc672b4afe45b1cd2
Gideon <gideon_hoog@hotmail.com>
2021-12-01 10:39:13 +0100

    solution day 1 2020

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -4,18 +4,31 @@ fun main() {
 
 class ProblemDay01:Problem("Day01") {
     override fun validatePart1TestInput(result: Int) {
-        check(result == 1)
+        check(result == 514579)
     }
 
     override fun validatePart2TestInput(result: Int) {
-        check(result == 1)
+        check(result == 241861950)
     }
 
     override fun part1(input:List<String>):Int {
-        return input.size
+        return input.toNumbers().findMatch(2020)!!.multiply()
+    }
+
+    private fun List<Int>.findMatch(goal:Int): List<Int>? {
+        return groupBy { it.toLower(goal) }.values.firstOrNull { it.size == 2 }?.firstOrNull()?.run {
+            listOf(this, this.flip(goal))
+        }
     }
 
     override fun part2(input:List<String>):Int {
-        return input.size
+        return with(input.toNumbers()) {
+            mapNotNull { findMatch(it.flip()) }.flatten().distinct().multiply()
+        }
     }
+
+    private fun Int.flip(goal:Int = 2020) = goal - this
+    private fun Int.toLower(goal: Int = 2020) = if(this > goal*0.5) flip(goal) else this
+    private fun Int.toResult(goal:Int = 2020) = this * this.flip(goal)
+    private fun List<Int>.multiply() = reduce {res, el -> res * el}
 }
