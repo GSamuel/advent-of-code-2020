51a594739478a2227da5113b122b06c57fed0db6
Gideon <gideon_hoog@hotmail.com>
2021-12-01 14:48:36 +0100

    solution day 6

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -2,21 +2,24 @@ fun main() {
     ProblemDay06().solve()
 }
 
-class ProblemDay06:Problem<Int>("Day06") {
+class ProblemDay06 : Problem<Int>("Day06") {
     override fun validatePart1TestInput(result: Int) {
-        check(result == 7)
+        check(result == 11)
     }
 
     override fun validatePart2TestInput(result: Int) {
-        check(result == 1)
+        check(result == 6)
     }
 
-    override fun part1(input:List<String>):Int {
-        return 0
+    override fun part1(input: List<String>): Int {
+        return input.joinToString(separator = "") { it.ifBlank { "|" } }.split("|").sumOf { it.toSet().size }
     }
 
-    override fun part2(input:List<String>):Int {
-        return 0
+    override fun part2(input: List<String>): Int {
+        return input.joinToString(separator = "_") { it.ifBlank { "|" } }.split("|").sumOf { group ->
+            group.toSet().filter { it != '_' }
+                .count { c -> group.split("_").filter { it.isNotBlank() }.all { it.contains(c) } }
+        }
     }
 
 }
