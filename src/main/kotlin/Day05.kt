0c7cc42e449a897352ea4c0b6ed48bc155d9b057
Gideon <gideon_hoog@hotmail.com>
2021-12-01 13:42:22 +0100

    solution day 5

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -4,18 +4,28 @@ fun main() {
 
 class ProblemDay05:Problem<Int>("Day05") {
     override fun validatePart1TestInput(result: Int) {
-        check(result == 7)
+        check(result == 820)
     }
 
     override fun validatePart2TestInput(result: Int) {
-        check(result == 1)
     }
 
     override fun part1(input:List<String>):Int {
-        return 0
+        return input.map { it.toSeatId() }.maxOf { it }
     }
 
     override fun part2(input:List<String>):Int {
-        return 0
+        val seats = input.map { it.toSeatId() }
+        val res = seats.filter { seatId -> (!seats.contains(seatId-1) || !seats.contains(seatId+1)) && seats.minByOrNull { it } != seatId&& seats.maxByOrNull { it } != seatId}
+        return res.sumOf { it }.div(res.size)
+    }
+
+    private fun String.toSeatId(): Int {
+        return map {
+            when(it) {
+                'B', 'R' -> 1
+                else -> 0
+            }
+        }.joinToString(separator = "").toInt(2)
     }
 }
