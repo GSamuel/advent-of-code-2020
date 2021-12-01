39eda14aef784882bdf044684d203a1caef8a0d1
Gideon <gideon_hoog@hotmail.com>
2021-12-01 10:56:36 +0100

    solution day 2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -4,7 +4,7 @@ fun main() {
 
 class ProblemDay02:Problem("Day02") {
     override fun validatePart1TestInput(result: Int) {
-        check(result == 1)
+        check(result == 2)
     }
 
     override fun validatePart2TestInput(result: Int) {
@@ -12,10 +12,21 @@ class ProblemDay02:Problem("Day02") {
     }
 
     override fun part1(input:List<String>):Int {
-        return input.size
+        return input.map { it.toPolicy() }.count { it.isValidRange() }
     }
 
     override fun part2(input:List<String>):Int {
-        return input.size
+        return input.map { it.toPolicy() }.count { it.isValidPosition() }
+    }
+
+    private fun String.toPolicy(): PasswordPolicy {
+        val (range, char, pass) = split(" ")
+        val (min, max) = range.split("-").map { it.toInt() }
+        return PasswordPolicy(min, max, char[0], pass)
+    }
+
+    class PasswordPolicy(val first:Int, val second:Int, val char:Char, val password:String) {
+        fun isValidRange():Boolean = password.count { it == char } in first..second
+        fun isValidPosition():Boolean = (password[first-1] == char) xor (password[second-1] == char)
     }
 }
